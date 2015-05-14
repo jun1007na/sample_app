d6c80c33852bb357d9a09f1134ae188d0516e205
jun1007na <junya.n.107+github@gmail.com>
2015-05-14 19:25:15 +0900

    8.2.1完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ require 'spec_helper'
 describe User do
 
     before do
-        @user = User.new(name: "Example User", email: "user@example.com",password: "foobar", password_confirmation: "foobar")
+        @user = User.new(name: "Example User", email: "user@example.com",
+            password: "foobar", password_confirmation: "foobar")
     end
 
     subject { @user }
@@ -14,6 +15,8 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
 
 
     it { should be_valid }
@@ -106,6 +109,11 @@ foo@bar_baz.com foo@bar+baz.com]
             @user.save
             expect(@user.reload.email).to eq mixed_case_email.downcase
         end
+    end
+
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
     end
 
 end
